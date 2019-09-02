5def8f4bf9c464048ac8a24d9cdd8c9fc9104f05
jean altahona <jpaltahonacc@gmail.com>
2019-09-02 07:08:39 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const expres = require('express');
+const app = expres();
+
+// Seting
+app.set('port', 5000);
+app.set('view', __dirname)
+
+//Middleware
+
+//Routes
+
+// Static
+
+module.exports = app;
