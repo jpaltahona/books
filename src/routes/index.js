b5d087defc7acb5556a298ce0d9041228072ee33
jean <jpaltahonacc@gmail.com>
2019-09-03 14:21:25 -0500

    app final con exprees

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,50 @@
 const { Router } = require('express');
+const fs = require('fs');
 const router = Router();
+const uuid = require('uuid/v4');
+
+
+const json_books = fs.readFileSync('./src/book.json', 'utf-8')
+var books = JSON.parse(json_books);
+
 
 router.get('/', (req, res, next) =>{
-    res.render('index.ejs')
+    res.render('index.ejs', {
+        books
+    })
 })
 
 router.get('/new-entry', (req, res) => {
     res.render('new-entry.ejs');
+})
+router.post('/new-entry', (req, res ) =>{
+    const { title, author, image, description } = req.body;
+    if(!title || !author || !image || !description ){
+        res.status(400).send('Entries must a title and description')
+        return
+    }
+
+    let newBook = {
+        id: uuid(),
+        title,
+        author,
+        image, description
+    }
+    books.push(newBook);
+
+    const json_book = JSON.stringify(books);
+    fs.writeFileSync('./src/book.json', json_book, 'utf-8');
+
+    res.redirect('/')
+})
+router.get('/delete/:id', (req, res ) =>{
+    console.log(req.params);
+
+    books = books.filter(book => book.id != req.params.id);
+
+    let json_books = JSON.stringify(books);
+    fs.writeFileSync('./src/book.json', json_books, 'utf-8');
+
+    res.redirect('/')
 })
 module.exports = router;
